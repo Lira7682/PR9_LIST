adaebe3200e16619a07ad59684083d77373904c6
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-25 20:44:22 -0600

    Add files via upload

---
+++ pr9_list.py
@@ -0,0 +1,41 @@
+
+print (" ")
+print ("Lira Hernandez Dayana Yamileth:")
+print ("muestra los miembros de las listas")
+print (" ")
+#Practica 8  donde usamos listas
+thislist = ["lapiz", "zapato", "lente", "mouse"] #muestra lo que presenta la variable
+print(thislist) #imprime los miembros de la lista
+
+#cuando usamos (len( )) te despliega
+#el numero de elementos de la lista
+thislist2 = ["lapiz", "zapato", "lente", "mouse"] #muestra lo que presenta la variable
+print(len(thislist))  #imprime los miembros de la lista
+
+
+#Separa con un espacio
+print(" ") #imprime un espacio
+#Da valores a varias listas
+list1 = ["lapiz", "zapato", "lente", "mouse"]#muestra los miembros de la lista
+list2 = [3, 5, 8, 2, 6]#muestra los miembros de la lista
+list3 = [True,False,False]#muestra los miembros de la lista
+list4 = ["xyz", 50, True, 13, "cp"]#muestra los miembros de la lista
+
+#Muestra los miembros de cada lista
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+
+
+print(" ")
+
+thislist = ["lapiz", "zapato", "lente", "mouse"]
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[3-4])
+
+print(" ")
